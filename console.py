96cab2da59392fc683e44a656602d664bbcb0c91
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2020-03-25 21:42:28 +0600

    Update console.py

--- console.py
+++ console.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import platform
 import time
 import traceback
 import json
@@ -7,22 +9,55 @@ from fbchat.models import *
 import wget
 from getpass import getpass
 import os.path
+import requests
 
+if os.name == 'nt':
+    path = str(os.environ["HOMEPATH"]) + "\\Desktop\\MessIm"
+elif os.name == "posix":
+    if os.path.exists("/storage/emulated/0"):
+        try:
+            os.mkdir('/storage/emulated/0/MessIm')
+        except:
+            pass
+        path = "/storage/emulated/0/MessIm"
+    elif platform.system() == 'Linux':
+        path = os.path.expanduser("~/Desktop")
+        if os.path.exists(path) == False:
+            path = input('\nEnter the full path of folder for saving images: ')
+            if path == '':
+                path = os.path.dirname(os.path.realpath(__file__))
+                os.chdir(path)
+                os.mkdir('MessIm')
+                path = path + "/MessIm"
+    else:
+        print("OS not detected.\nEnter the full path of folder for saving images: ")
+        if path == '':
+            path = os.path.dirname(os.path.realpath(__file__))
+            os.chdir(path)
+            os.mkdir('MessIm')
+            path = path + "/MessIm"
+                
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 print(os.path.basename(__file__))
 
-ua_file_path = dir_path + "\\ua.txt"
+ua_file_path = dir_path + "/ua.txt"
 
 print("##################################\nMessenger Image Bulk Downloader\n##################################\n")
 print("Not only images, you can download files from Messenger by reacting to messages, forwarding to your own conversation or just simply save all whoever sends you something.")
-print("Images, GIFs, Videos and other attachments will be saved at \"MessIm\" folder in Desktop.\n")
+print("Images, GIFs, Videos and other attachments will be saved at " + path + " directory in Desktop.\n")
 option = int(input("Select an option:\n1) Save all images in one folder.\n2) Save images conversation name-wise.\n3) Save images conversation name-wise and time-wise.\n\nYour Choice: "))
 
 
-if os.path.exists(ua_file_path) == False: 
-    user_agent = input('\n\nPlease enter the user agent of the browser you usually login to facebook. Just goto https://www.whatsmyua.info/, copy the text, paste here and press enter.\n\nYour browser\'s user agent will be something like this:\nMozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36\n\nUser agent is needed to fake this program as a normal browser you use, so that your account remains human-like to Facebook. You\'ll not have to enter the user agent every time you use this program. :-)\n\nUser agent: ')
+if os.path.exists(ua_file_path) == False:
+    words = "\n\nPlease enter the user agent of the browser you usually login to facebook. If you usually use Facebook App or Messenger App, then just use the browser's user agent.\n\n Just goto https://www.whatsmyua.info/, copy the text, paste here and press enter.\n\nYour browser\'s user agent will be something like this:\nMozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36\n\nUser agent is needed to fake this program as a normal browser you use, so that your account remains human-like to Facebook. You\'ll not have to enter the user agent every time you use this program. :-)"
+    for char in words:
+        time.sleep(0.01)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        
+    user_agent = input('\n\nUser agent: ')
     with open(ua_file_path, "w") as ua:
         ua.write(user_agent)
 else:
@@ -33,7 +68,7 @@ else:
 count = 0
 
 def download(option, url, threadname, time):
-    path = str(os.environ["HOMEPATH"]) + "\\Desktop\\MessIm"
+    global path
     try:
         os.mkdir(path)
     except:
@@ -43,40 +78,44 @@ def download(option, url, threadname, time):
         os.chdir(path)
         wget.download(url)
     elif option == 2:
+        os.chdir(path)
         try:
-            os.mkdir(path + "\\" + threadname)
+            os.mkdir(threadname)
         except:
             pass
-        os.chdir(path + "\\" + threadname)
+        os.chdir(threadname)
         wget.download(url)
     elif option == 3:
+        os.chdir(path)
         try:
-            os.mkdir(path + "\\" + threadname)
+            os.mkdir(threadname)
         except:
             pass
-        
+        os.chdir(threadname)
         try:
-            os.mkdir(path + "\\" + threadname + "\\" + time)
+            os.mkdir(time)
         except:
             pass
+
             
-            
-        os.chdir(path + "\\" + str(threadname) + "\\" + time)
+        os.chdir(time)
         wget.download(url)
         
         
 
 def login_logout():
+    global user_agent
+    print(user_agent)
     cookies = {}
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    cookies_path = dir_path + "\\session.json"
+    cookies_path = dir_path + "/session.json"
     try:
         # Load the session cookies
         if os.path.isfile(cookies_path):
             with open(cookies_path, 'r') as f:
                 cookies = json.load(f)
     except:
-        os.remove('session.json')
+        os.remove(cookies_path)
         # If it fails, never mind, we'll just login again
         # client = CustomClient(email, password, max_tries=1)
                 
@@ -85,15 +124,14 @@ def login_logout():
         client = CustomClient('email', 'password', user_agent=user_agent, session_cookies=cookies, logging_level = 30, max_tries=1)
 
     else:
-        print("\n\nSaved credentials not found...\n\nEnter your Facebook credentials.\n(Don't worry, password will not be visible when typing! ;-) ")
-        email = input("Email/Phone/UserID: ")
+        print("\n\nSaved credentials not found...\n\nEnter your Facebook credentials.\n\n(Don't worry, password will not be visible when typing! ;-) ")
+        email = input("\nEmail/Phone/UserID: ")
         password = getpass("Password: ")
         print("\nIf you have Two Factor Authentication enabled in your account, enter then code when prompted.\n")
 
-        
         client = CustomClient(email, password, user_agent=user_agent, logging_level = 30, max_tries=1)
     
-    with open('session.json', 'w') as f:
+    with open(cookies_path, 'w') as f:
         json.dump(client.getSession(), f)
 
     print("\nLogged in successfully\nListening for actions...")
@@ -140,8 +178,14 @@ def getMessageContent(self, t, messageObject, threadname):
             elif hasattr(messageObject.attachments[i], 'url') and messageObject.attachments[i].url != None:
 
                 url = str(messageObject.attachments[i].url)
-
+                # print(url)
                 down_print(count)
+                # os.chdir(path)
+                # r = requests.head(url, allow_redirects=True)
+                # print(r.url)
+                # with open('file_name.pdf', 'wb') as f:
+                #     f.write(r.content)
+                # download(option, r, threadname, time1)
                 download(option, url, threadname, time1)
 
             # If video
